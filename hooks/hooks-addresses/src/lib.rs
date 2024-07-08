3eff7f8f550bd98b8f32aae1c0d16d3fdf1d91bb
Sam Fisher <5th@echelon>
2024-07-09 01:06:30 +0200

    Add support for ultrawide
    Fixes #40

--- hooks/hooks-addresses/src/lib.rs
+++ hooks/hooks-addresses/src/lib.rs
@@ -159,6 +159,95 @@ fn dx9_addresses() -> HashMap<[u8; 32], Addresses> {
             0xbd, 0x42, 0x76, 0x41, 0x8, 0xe7, 0xdf, 0x37, 0x4, 0x9f, 0x14, 0x90, 0xf2, 0x9, 0x29,
             0xf9, 0x92, 0x5d,
         ],
+        /*
+        Byte Diffs :
+            Address   Program1  Program2
+            004001a8    0xe7       0x26
+            004001a9    0x68       0x3c
+            Address   Program1  Program2
+            004d0c73    0xf2       0xe9
+            004d0c74    0x0f       0x42
+            004d0c75    0x59       0x08
+            004d0c76    0x05       0x0c
+            004d0c77    0x80       0x02
+            004d0c78    0x70       0x90
+            004d0c79    0xd5       0x90
+            004d0c7a    0x02       0x90
+            Address   Program1  Program2
+            004d0c91    0xf3       0xe9
+            004d0c92    0x0f       0x3b
+            004d0c93    0x10       0x08
+            004d0c94    0x46       0x0c
+            004d0c95    0x40       0x02
+            Address   Program1  Program2
+            006c08e7    0xf3       0xe9
+            006c08e8    0x0f       0x66
+            006c08e9    0x10       0x0b
+            006c08ea    0x05       0xed
+            006c08eb    0xc8       0x01
+            006c08ec    0x4e       0x90
+            006c08ed    0x23       0x90
+            006c08ee    0x03       0x90
+            Address   Program1  Program2
+            006e17d6    0xf3       0xe9
+            006e17d7    0x0f       0x7e
+            006e17d8    0x10       0x64
+            006e17d9    0x45       0x25
+            006e17da    0x20       0x02
+            Address   Program1  Program2
+            0071a4f0    0x0f       0xe9
+            0071a4f1    0x84       0xcb
+            0071a4f2    0xca       0x01
+            0071a4f3    0x01       0x00
+            0071a4f5    0x00       0x90
+            Address   Program1  Program2
+            007eb33a    0xf2       0xe9
+            007eb33b    0x0f       0x77
+            007eb33c    0x10       0xc9
+            007eb33d    0x0d       0x14
+            007eb33e    0xe8       0x02
+            007eb33f    0x70       0x90
+            007eb340    0xd5       0x90
+            007eb341    0x02       0x90
+            Address   Program1  Program2
+            0090eafa    0x83       0xe9
+            0090eafb    0xbb       0x68
+            0090eafc    0x34       0x29
+            0090eafd    0x01       0xc8
+            0090eafe    0x00       0x01
+            0090eaff    0x00       0x90
+            0090eb00    0x01       0x90
+            Address   Program1  Program2
+            00b27b19    0x00       0x11
+            Address   Program1  Program2
+            00d18bdf    0x76       0xeb
+            Address   Program1  Program2
+            00d198e0    0x10       0x00
+            Address   Program1  Program2
+            00fd1423    0xf3       0xe9
+            00fd1424    0x0f       0x5d
+            00fd1425    0x10       0x00
+            00fd1426    0x46       0x5c
+            00fd1427    0x34       0x01
+            Address   Program1  Program2
+            0195aff5    0x07       0x08
+            0195aff6    0xaf       0xcc
+            0195aff7    0x07       0xfd
+            Address   Program1  Program2
+            019d8040    0xf3       0xe9
+            019d8041    0x0f       0xf5
+            019d8042    0x10       0xfb
+            019d8043    0x0d       0xf5
+            019d8044    0x6c       0x00
+            019d8045    0xf3       0x90
+            019d8046    0x94       0x90
+            019d8047    0x02       0x90
+         */
+        [
+            0xaf, 0x06, 0x72, 0x09, 0x45, 0x88, 0x04, 0xe6, 0xad, 0xce, 0xca, 0x43, 0x2b, 0x50,
+            0xfb, 0x25, 0x52, 0x08, 0x71, 0x6f, 0xb2, 0x30, 0x37, 0xe8, 0x25, 0x24, 0x4d, 0xbc,
+            0x9f, 0x3b, 0x19, 0x5c,
+        ],
     ];
 
     HashMap::from(dx9_hashes.map(|h| (h, dx9_addrs.clone())))
@@ -183,6 +272,94 @@ fn dx11_addresses() -> HashMap<[u8; 32], Addresses> {
             0xc5, 0x2b, 0x3d, 0x09, 0x27, 0x59, 0x1e, 0x47, 0x74, 0x24, 0xf3, 0x89, 0xff, 0x0b,
             0x13, 0x14, 0xa3, 0x00, 0x93, 0x8e, 0x19, 0xce, 0x61, 0xa7, 0xb0, 0xa7, 0xbc, 0x09,
             0xf8, 0x1c, 0x2c, 0x89,
+        ],
+        /*
+        Byte Diffs :
+            Address   Program1  Program2
+            00400198    0x51       0x69
+            00400199    0x98       0x97
+            Address   Program1  Program2
+            0042ccbe    0xf3       0xe9
+            0042ccbf    0x0f       0xea
+            0042ccc0    0x10       0x5c
+            0042ccc1    0x45       0x54
+            0042ccc2    0x20       0x02
+            Address   Program1  Program2
+            0046c860    0x0f       0xe9
+            0046c861    0x84       0xcb
+            0046c862    0xca       0x01
+            0046c863    0x01       0x00
+            0046c865    0x00       0x90
+            Address   Program1  Program2
+            00542aea    0xf2       0xe9
+            00542aeb    0x0f       0x58
+            00542aec    0x10       0x77
+            00542aed    0x0d       0x08
+            00542aee    0x60       0x02
+            00542aef    0x5c       0x90
+            00542af0    0xd7       0x90
+            00542af1    0x02       0x90
+            Address   Program1  Program2
+            0066fe91    0x83       0xe9
+            0066fe92    0xbb       0x51
+            0066fe93    0x34       0x14
+            0066fe94    0x01       0x30
+            0066fe95    0x00       0x02
+            0066fe96    0x00       0x90
+            0066fe97    0x01       0x90
+            Address   Program1  Program2
+            0088cfc9    0x00       0x11
+            Address   Program1  Program2
+            00a7f7ff    0x76       0xeb
+            Address   Program1  Program2
+            00a80670    0x10       0x00
+            Address   Program1  Program2
+            00d3b853    0xf3       0xe9
+            00d3b854    0x0f       0xc4
+            00d3b855    0x10       0x5a
+            00d3b856    0x46       0xc3
+            00d3b857    0x34       0x01
+            Address   Program1  Program2
+            016db3c8    0x54       0xc5
+            016db3c9    0x2b       0x5e
+            016db3ca    0x08       0x29
+            016db3cb    0x00       0x01
+            Address   Program1  Program2
+            0175f4b0    0xf3       0xe9
+            0175f4b1    0x0f       0xfc
+            0175f4b2    0x10       0x1e
+            0175f4b3    0x0d       0x21
+            0175f4b4    0x00       0x01
+            0175f4b5    0x5c       0x90
+            0175f4b6    0xd7       0x90
+            0175f4b7    0x02       0x90
+            Address   Program1  Program2
+            018f20a3    0xf2       0xe9
+            018f20a4    0x0f       0xc7
+            018f20a5    0x59       0xf2
+            018f20a6    0x05       0x07
+            018f20a7    0x50       0x01
+            018f20a8    0xf9       0x90
+            018f20a9    0xc1       0x90
+            018f20aa    0x02       0x90
+            Address   Program1  Program2
+            018f20c1    0xf3       0xe9
+            018f20c2    0x0f       0xc0
+            018f20c3    0x10       0xf2
+            018f20c4    0x46       0x07
+            018f20c5    0x40       0x01
+            Address   Program1  Program2
+            01acb93a    0xf3       0xe9
+            01acb93b    0x0f       0xc6
+            01acb93c    0x10       0x59
+            01acb93d    0x46       0xea
+            01acb93e    0x50       0x00
+
+         */
+        [
+            0xb5, 0x21, 0xc4, 0xb0, 0x23, 0x62, 0x72, 0xe7, 0x3f, 0xcb, 0xf9, 0x35, 0xdb, 0x33,
+            0x80, 0xa8, 0x4b, 0x7c, 0x0f, 0xa9, 0x80, 0x47, 0x66, 0x43, 0x77, 0xdb, 0x1c, 0xda,
+            0xaf, 0xde, 0x06, 0x7f,
         ],
     ];
 
